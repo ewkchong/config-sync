c46f59a1dda1194c8b87422af492d44b6788a9f6
Edward Chong <82353320+ewkchong@users.noreply.github.com>
2022-06-17 17:22:55 -0700

    allow differently named remote repo

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ git -C $REPO_PATH diff --exit-code -s
 if [ $? -eq 1 ]; then
     read -p "There is an update available for configsync, would you like to update? [yes/no]: " ans
     if [ "$ans" = "yes" ]; then
-        git -C $REPO_PATH reset --hard -q origin/scp 
+        git -C $REPO_PATH reset --hard -q $(git remote)/master 
         cp "${REPO_PATH}/configsync.sh" /usr/local/bin/configsync.sh
         SUCCESS=" \x1B[1;97;42m[success]\x1B[22;0m "
         echo "configsync has been updated. Running command..."
