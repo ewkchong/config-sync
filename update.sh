b1bc1018cde26b386cf50b89bbd133e281762743
Edward Chong <82353320+ewkchong@users.noreply.github.com>
2022-06-17 17:17:58 -0700

    update update script

--- update.sh
+++ update.sh
@@ -4,15 +4,11 @@ git -C $REPO_PATH diff --exit-code -s
 if [ $? -eq 1 ]; then
     read -p "There is an update available for configsync, would you like to update? [yes/no]: " ans
     if [ "$ans" = "yes" ]; then
-        git -C $REPO_PATH reset --hard -q origin/staging 
+        git -C $REPO_PATH reset --hard -q origin/scp 
         cp "${REPO_PATH}/configsync.sh" /usr/local/bin/configsync.sh
         SUCCESS=" \x1B[1;97;42m[success]\x1B[22;0m "
-        read -p "${SUCCESS}configsync has been updated. Would you like to continue with your command: configsync $1? [yes/no]" confirm
-        if [ "$confirm" = "yes"]; then
-            exit 0;
-        else
-            exit 1;
-        fi
+        echo "configsync has been updated. Running command..."
+        exit 0;
     else
         exit 1;
     fi
