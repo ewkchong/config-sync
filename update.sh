fed4b086385ca09ef9e55990ff281ec862d98b69
Edward Chong <82353320+ewkchong@users.noreply.github.com>
2022-06-20 13:40:47 -0700

    update update script

--- update.sh
+++ update.sh
@@ -6,13 +6,16 @@ if [ $? -eq 1 ]; then
     if [ "$ans" = "yes" ]; then
         git -C $REPO_PATH reset --hard -q $(git remote)/master 
         cp "${REPO_PATH}/configsync.sh" /usr/local/bin/configsync.sh
+        if [ $? -eq 1 ]; then
+            echo "Unable to update configsync. Please check your permissions or ensure the path to the repository is correct in ~/.configsync_info"
+            exit 1;
+        fi
         SUCCESS=" \x1B[1;97;42m[success]\x1B[22;0m "
         echo "configsync has been updated. Running command..."
         exit 0;
     else
-        exit 1;
+        exit 0;
     fi
 else
-    :
-    exit 1;
+    exit 0;
 fi
