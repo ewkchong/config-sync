6d87818c6825c1a581880cb0f75cb131ca119664
Rafi Meher <rafi.meher@ubc.ca>
2023-05-16 09:43:59 -0700

    add checks to confirm uninstall success
    If there are permission issues, uninstall.sh may fail but show a success message. This checks if all items have been removed properly

--- uninstall.sh
+++ uninstall.sh
@@ -7,4 +7,14 @@ fi
 if [ -e ~/.configsync_info ]; then
     rm ~/.configsync_info
 fi
-echo "Files associated with configsync have been removed."
+# Confirm that files have been removed
+if [ -e /usr/local/bin/configsync ]  || [ -e /usr/local/bin/configsync-update ] || [ -e ~/.configsync_info ]; then
+    echo "\n Unable to remove one or all of the following:
+    • '/usr/local/bin/configsync'
+    • '/usr/local/bin/configsync-update'
+    • '~/.configsync_info'
+        \n You may try running this command with root privileges, or by manually removing these items. Exiting with error. \n"
+    exit 1;
+else
+    echo "Files associated with configsync have been removed."
+fi
